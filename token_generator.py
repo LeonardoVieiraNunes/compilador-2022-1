4898e33a2910b01897482d2dc784dee98ab49a84
LeonardoVieiraNunes <devleonardonunes@gmail.com>
2022-05-08 16:53:02 -0300

    creating tokens

---
+++ token_generator.py
@@ -0,0 +1,86 @@
+# tokens list
+tokens = ('PLUS',
+          'MINUS',
+          'TIMES',
+          'DIVIDE'
+          'MOD',
+          'ASSIGN',
+          'LT',
+          'GT',
+          'LTE',
+          'GTE',
+          'EQUAL',
+          'INEQUAL',
+          'DOT',
+          'COMMA',
+          'SEMICOLON',
+          'RIGHTPARENTHESES',
+          'LEFTPARENTHESES',
+          'RIGHTBRACKET',
+          'LEFTBRACKET',
+          'RIGHTBRACE',
+          'LEFTBRACE',
+          'DIGITO',
+          'LETRA',
+          'CHARESPECIAL',
+          'INTCONSTANT',
+          'FLOATCONSTANT',
+          'STRINGCONSTANT',
+          'IDENT',
+          'DEF',
+          'INT',
+          'FLOAT',
+          'STRING',
+          'BREAK',
+          'PRINT',
+          'READ',
+          'RETURN',
+          'IF',
+          'ELSE',
+          'FOR',
+          'NEW',
+          'NULL'
+          )
+
+#regular expression
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_MOD = r'\%'
+t_ASSIGN
+t_LT
+t_GT
+t_LTE
+t_GTE
+t_EQUAL
+t_INEQUAL
+t_DOT
+t_COMMA
+t_SEMICOLON
+t_RIGHTPARENTHESES
+t_LEFTPARENTHESES
+t_RIGHTBRACKET
+t_LEFTBRACKET
+t_RIGHTBRACE
+t_LEFTBRACE
+t_DIGITO
+t_LETRA
+t_CHARESPECIAL
+t_INTCONSTANT
+t_FLOATCONSTANT
+t_STRINGCONSTANT
+t_IDENT
+t_DEF
+t_INT
+t_FLOAT
+t_STRING
+t_BREAK
+t_PRINT
+t_READ
+t_RETURN
+t_IF
+t_ELSE
+t_FOR
+t_NEW
+t_NULL
