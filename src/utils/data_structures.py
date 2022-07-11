113a0ee2832b69e9f61992332f58ebc9957f21cb
AugustoCoelho <augustovcr@gmail.com>
2022-07-11 19:41:10 -0300

    start semantic analysis

--- src/utils/data_structures.py
+++ src/utils/data_structures.py
@@ -84,3 +84,113 @@ class SymbolRow:
 
 
 SymbolTableType = Dict[str, SymbolRow]
+
+@dataclass
+class TableEntry:
+    identifier_label: str
+    datatype: str
+    dimesions: List[int]
+    line: int
+
+    def as_json(self):
+        return {
+            'identifier_label': self.identifier_label,
+            'datatype': self.datatype,
+            'dimesions': self.dimesions,
+            'line': self.line
+        }
+
+
+class Scope:
+    def __init__(self, upper_scope=None, is_loop: bool = False):
+        self.table: List[TableEntry] = []
+
+        self.upper_scope = upper_scope
+
+        self.inner_scopes = []
+
+        self.is_loop = is_loop
+
+    def add_entry(self, entry: TableEntry):
+        is_present, line_declared = self.var_already_present(
+            entry.identifier_label)
+        if is_present:
+            raise VariableAlreadyDeclaredInScopeError(line_declared)
+        self.table.append(entry)
+
+    def var_already_present(self, ident):
+        for entry in self.table:
+            if entry.identifier_label == ident:
+                return True, entry.line
+
+        return False, 0
+
+    def add_inner(self, scope):
+        self.inner_scopes.append(scope)
+
+    def __str__(self):
+        return '\n'.join([
+            str(entry)
+            for entry in self.table
+        ]) + '\n'
+
+    def as_json(self) -> Dict:
+        return {
+            'table': [
+                entry.as_json() for entry in self.table
+            ],
+            'inner_scopes': [scope.as_json() for scope in self.inner_scopes]
+        }
+
+
+class ScopeStack:
+    def __init__(self):
+        self.stack = []
+
+    def pop(self):
+        return self.stack.pop()
+
+    def push(self, scope: Scope):
+        self.stack.append(scope)
+
+    def seek(self) -> Scope:
+        if self.stack:
+            return self.stack[-1]
+
+        else:
+            return None
+
+    def __len__(self):
+        return len(self.stack)
+
+
+@dataclass
+class Node:
+
+    def __init__(self, left: Optional['Node'], right: Optional['node'],
+                 value: Optional[Union[str, int, float]], result_type: str):
+        self.left = left
+        self.right = right
+        self.value = value
+
+        self.result_type = result_type
+
+        self.id = uuid.uuid4()
+
+    def as_json(self) -> Dict:
+        left = None
+        if self.left is not None:
+            left = self.left.as_json()
+
+        right = None
+        if self.right is not None:
+            right = self.right.as_json()
+
+        return {
+            'value': self.value,
+            'left': left,
+            'right': right
+        }
+
+    def __str__(self):
+        return f'<NodeId: {self.id}>'
